055a427714f188f692aab724f51208026d7a6f9a
大橙子 <574445631@qq.com>
2024-12-28 11:51:55 +0800

    实现接口调用

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	"DataMapper/config"
-	"fmt"
+	"DataMapper/internal/api"
 )
 
 func main() {
-	conf, err := config.LoadMapperConfig("config/mapper_config_template.json")
-	if err != nil {
-		fmt.Errorf("报错了")
-	}
-	/*	var fileImporter _import.DataImport
-		fileImporter = _import.CSVImporter{}
-		data, err := fileImporter.Importer("C:\\Users\\alvhua\\Documents\\数据处理\\Global_2023-07-22_Customer_GPOS_01_00.csv")
-		if err != nil {
-			return
-		}*/
-	fmt.Println(conf)
+	request := api.Request{Url: "http://localhost:8080/get", Params: map[string]string{"id": "12345"}, Method: api.GET, Body: ""}
+	request.Do()
 }
--- internal/api/apiClient.go
+++ internal/api/apiClient.go
@@ -1,3 +1,68 @@
 package api
 
-// 这里处理api 接口请求1
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const (
+	GET  = "GET"
+	POST = "POST"
+)
+
+// Request 请求体
+type Request struct {
+	Url     string
+	Params  map[string]string
+	Headers map[string]string
+	Method  string
+	Body    string
+}
+
+// Do 发送请求
+func (r *Request) Do() {
+	if r.Method == GET {
+		// TODO: 实现GET请求
+		resp, err := http.Get(buildGetUrl(r.Url, r.Params))
+		if err != nil {
+			// TODO: 处理错误
+		}
+		defer resp.Body.Close()
+		// 读取响应
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// 打印响应
+		fmt.Println(string(body))
+	} else {
+		// TODO: 实现POST请求
+		resp, err := http.Post(r.Url, "application/json", bytes.NewBufferString(r.Body))
+		if err != nil {
+			// TODO: 处理错误
+		}
+		defer resp.Body.Close()
+		// 读取响应
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// 打印响应
+		fmt.Println(string(body))
+	}
+
+}
+
+func buildGetUrl(url string, params map[string]string) string {
+	if len(params) == 0 {
+		return url
+	}
+	url += "?"
+	for k, v := range params {
+		url += k + "=" + v + "&"
+	}
+	return url[:len(url)-1]
+}
