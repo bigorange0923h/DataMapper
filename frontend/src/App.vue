ef9dc5cc86d751416db9b064a7fe74aede8aad7c
大橙子 <574445631@qq.com>
2025-01-08 15:57:17 +0800

    UI 界面基础模型

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,115 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'</script>
-
 <template>
-  <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png"/>
-  <HelloWorld/>
-</template>
 
-<style>
-#logo {
-  display: block;
-  width: 50%;
-  height: 50%;
-  margin: auto;
-  padding: 10% 0 0;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  background-origin: content-box;
+  <a-layout style="min-height: 100vh">
+    <a-layout-sider v-model:collapsed="collapsed" collapsible>
+      <div class="logo"/>
+      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <pie-chart-outlined/>
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <desktop-outlined/>
+          <span>Option 2</span>
+        </a-menu-item>
+        <a-sub-menu key="sub1">
+          <template #title>
+            <span>
+              <user-outlined/>
+              <span>User</span>
+            </span>
+          </template>
+          <a-menu-item key="3">Tom</a-menu-item>
+          <a-menu-item key="4">Bill</a-menu-item>
+          <a-menu-item key="5">Alex</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+            <span>
+              <team-outlined/>
+              <span>Team</span>
+            </span>
+          </template>
+          <a-menu-item key="6">Team 1</a-menu-item>
+          <a-menu-item key="8">Team 2</a-menu-item>
+        </a-sub-menu>
+        <a-menu-item key="9">
+          <file-outlined/>
+          <span>File</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-row>
+        <a-col :span="24">col</a-col>
+      </a-row>
+      <a-layout-header class="header top">
+        <a-menu
+            v-model:selectedKeys="selectedKeys1"
+            theme="dark"
+            mode="horizontal"
+            :style="{ lineHeight: '64px' }"
+        >
+          <a-menu-item key="1">nav 1</a-menu-item>
+          <a-menu-item key="2">nav 2</a-menu-item>
+          <a-menu-item key="3">nav 3</a-menu-item>
+        </a-menu>
+      </a-layout-header>
+      <a-layout-content style="margin: 0 16px">
+        <!--目录路径-->
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+          <a-breadcrumb-item>App</a-breadcrumb-item>
+        </a-breadcrumb>
+        <!--API-->
+        <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+          <a-space>
+            <a-input placeholder="请输入接口地址" style="min-width: 110vh;">
+              <template #addonBefore>
+                <a-select v-model:value="apiType" style="width: 90px">
+                  <a-select-option value="POST">POST</a-select-option>
+                  <a-select-option value="GET">GET</a-select-option>
+                </a-select>
+              </template>
+            </a-input>
+            <a-button type="primary">导入</a-button>
+          </a-space>
+          <a-divider />
+          <a-space>
+            <RequestParamMapper/>
+          </a-space>
+        </div>
+        <!--入参-->
+
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        footer
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
+</template>
+<script setup>
+import {ref} from 'vue';
+import RequestParamMapper from "./components/RequestParamMapper.vue";
+
+const collapsed = ref(false);
+const selectedKeys = ref(['1']);
+const apiType = ref('GET')
+</script>
+<style scoped>
+.logo {
+  height: 32px;
+  margin: 16px;
+  background: rgba(255, 255, 255, 0.3);
 }
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
+}
+
 </style>
---
+++ frontend/src/components/RequestParamMapper.vue
@@ -0,0 +1,75 @@
+<script setup>
+const columns = [
+  {
+    title: '原始入参名称',
+    dataIndex: 'sourceName',
+    key: 'sourceName',
+  },
+  {
+    title: '原始入参类型',
+    dataIndex: 'sourceType',
+    key: 'sourceType',
+  },
+  {
+    title: '目标参数名称',
+    dataIndex: 'targetName',
+    key: 'targetName',
+  },
+  {
+    title: '目标参数类型',
+    dataIndex: 'targetType',
+    key: 'targetType',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+  },
+];
+const data = [
+  {
+    sourceName: 'name',
+    sourceType: 'string',
+    targetName: 'name',
+    targetType: 'string',
+  },
+  {
+    sourceName: 'age',
+    sourceType: 'integer',
+    targetName: 'age',
+    targetType: 'integer',
+  },
+  {
+    sourceName: 'sex',
+    sourceType: 'integer',
+    targetName: 'sex',
+    targetType: 'integer',
+  },
+  {
+    sourceName: 'nickName',
+    sourceType: 'string',
+    targetName: 'nickName',
+    targetType: 'string',
+  },
+  {
+    sourceName: 'mobile',
+    sourceType: 'string',
+    targetName: 'mobile',
+    targetType: 'string',
+  } ,
+  {
+    sourceName: 'phone',
+    sourceType: 'string',
+    targetName: 'mobile',
+    targetType: 'phone',
+  }
+
+]
+</script>
+
+<template>
+  <a-table :columns="columns" :data-source="data" :pagination="false" :scroll="{  y: 300 }"/>
+</template>
+
+<style scoped>
+
+</style>
