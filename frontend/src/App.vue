b060198180d9135afa2a8e627e63099d63e5ab26
大橙子 <574445631@qq.com>
2025-02-02 11:00:33 +0800

    替换为卡片

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
 
   <a-layout style="min-height: 100vh">
-    <a-layout-sider v-model:collapsed="collapsed" collapsible>
+    <a-layout-sider v-model:collapsed="collapsed" collapsible style="background: #fff"  @collapse="onCollapse" >
       <div class="logo"/>
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+      <a-menu v-model:selectedKeys="selectedKeys" theme="light" mode="inline">
         <a-menu-item key="1">
           <pie-chart-outlined/>
           <span>Option 1</span>
@@ -43,17 +43,12 @@
       <a-row>
         <a-col :span="24">col</a-col>
       </a-row>
-      <a-layout-header class="header top">
-        <a-menu
-            v-model:selectedKeys="selectedKeys"
-            theme="dark"
-            mode="horizontal"
-            :style="{ lineHeight: '64px' }"
-        >
-          <a-menu-item key="1">nav 1</a-menu-item>
-          <a-menu-item key="2">nav 2</a-menu-item>
-          <a-menu-item key="3">nav 3</a-menu-item>
-        </a-menu>
+      <a-layout-header class="header top"  style="background: #fff">
+        <a-tabs v-model:activeKey="activeKey" type="editable-card" @edit="onEdit">
+          <a-tab-pane v-for="pane in panes" :key="pane.key" :tab="pane.title" :closable="pane.closable">
+            {{ pane.content }}
+          </a-tab-pane>
+        </a-tabs>
       </a-layout-header>
       <a-layout-content style="margin: 0 16px">
         <!--目录路径-->
@@ -103,10 +98,30 @@ const apiType = ref('GET')
 // 引用子组件
 const importStart = async () => {
   console.log(RequestComponent.value.selectFilePath);
-  
+
 }
+const onCollapse = (collapseState) => {
+  collapsed.value = collapseState;
+};
 
-
+const panes = ref([
+  {
+    title: 'Tab 1',
+    content: 'Content of Tab 1',
+    key: '1',
+  },
+  {
+    title: 'Tab 2',
+    content: 'Content of Tab 2',
+    key: '2',
+  },
+  {
+    title: 'Tab 3',
+    content: 'Content of Tab 3',
+    key: '3',
+    closable: false,
+  },
+]);
 </script>
 <style scoped>
 .logo {
@@ -119,8 +134,26 @@ const importStart = async () => {
   background: #fff;
 }
 
-[data-theme='dark'] .site-layout .site-layout-background {
-  background: #141414;
+.ant-layout-sider-trigger {
+  background: #fff;
+}
+.a-layout-sider-trigger {
+  background-color: #f0f0f0 !important; /* 与 light 主题保持一致 */
+  color: #000 !important;              /* 设置文字颜色 */
+  border-top: 1px solid #d9d9d9;       /* 添加边框 */
 }
 
+
+.ant-layout .ant-layout-sider-trigger {
+   position: fixed;
+   bottom: 0;
+   z-index: 1;
+   height: 48px;
+   color: pink !important;
+   line-height: 48px;
+   text-align: center;
+   background: #fff !important;
+   cursor: pointer;
+   transition: all 0.2s;
+ }
 </style>
