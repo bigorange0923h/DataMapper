b53b31aca2e910280ad2193906a632a5b8e8a5f2
大橙子 <574445631@qq.com>
2025-02-06 16:56:18 +0800

    菜单加载

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,41 +1,29 @@
 <template>
 
   <a-layout style="min-height: 100vh">
-    <a-layout-sider v-model:collapsed="collapsed" collapsible style="background: #fff"  @collapse="onCollapse" >
+    <a-layout-sider v-model:collapsed="collapsed" collapsible style="background: #fff" @collapse="onCollapse">
       <div class="logo"/>
       <a-menu v-model:selectedKeys="selectedKeys" theme="light" mode="inline">
-        <a-menu-item key="1">
-          <pie-chart-outlined/>
-          <span>Option 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <desktop-outlined/>
-          <span>Option 2</span>
-        </a-menu-item>
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span>
-              <user-outlined/>
-              <span>User</span>
-            </span>
+        <template v-for="item in menuItems" :key="item.key">
+          <template v-if="item.children">
+            <a-sub-menu :key="item.key">
+              <template #title>
+                <span>{{ item.title }}</span>
+              </template>
+              <a-menu-item v-for="child in item.children" :key="child.key">
+                {{ child.title }}
+              </a-menu-item>
+            </a-sub-menu>
           </template>
-          <a-menu-item key="3">Tom</a-menu-item>
-          <a-menu-item key="4">Bill</a-menu-item>
-          <a-menu-item key="5">Alex</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span>
-              <team-outlined/>
-              <span>Team</span>
-            </span>
+          <template v-else>
+            <a-menu-item :key="item.key">
+              {{ item.title }}
+            </a-menu-item>
           </template>
-          <a-menu-item key="6">Team 1</a-menu-item>
-          <a-menu-item key="8">Team 2</a-menu-item>
-        </a-sub-menu>
-        <a-menu-item key="9">
-          <file-outlined/>
-          <span>File</span>
+        </template>
+        <a-menu-item key="add">
+          <PlusOutlined />
+          <span>新建</span>
         </a-menu-item>
       </a-menu>
     </a-layout-sider>
@@ -43,39 +31,32 @@
       <a-row>
         <a-col :span="24">col</a-col>
       </a-row>
-      <a-layout-header class="header top"  style="background: #fff">
-        <a-tabs v-model:activeKey="activeKey" type="editable-card" @edit="onEdit">
-          <a-tab-pane v-for="pane in panes" :key="pane.key" :tab="pane.title" :closable="pane.closable">
-            {{ pane.content }}
-          </a-tab-pane>
-        </a-tabs>
+      <a-layout-header class="header top" style="background: #fff">
+
       </a-layout-header>
       <a-layout-content style="margin: 0 16px">
-        <!--目录路径-->
-        <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
-        <!--API-->
-        <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
-          <a-space>
-            <a-input placeholder="请输入接口地址" style="min-width: 110vh;">
-              <template #addonBefore>
-                <a-select v-model:value="apiType" style="width: 90px">
-                  <a-select-option value="POST">POST</a-select-option>
-                  <a-select-option value="GET">GET</a-select-option>
-                </a-select>
-              </template>
-            </a-input>
-            <a-button type="primary" @click="importStart">开始导入</a-button>
-          </a-space>
-          <a-flex :vertical="value === 'vertical'">
-            <RequestParamMapper ref="RequestComponent"/>
-            <ImportResponse/>
-          </a-flex>
-        </div>
-        <!--入参-->
+        <a-tabs v-model:activeKey="activeKey" type="editable-card" @edit="onEdit">
+          <a-tab-pane v-for="pane in panes" :key="pane.key" :tab="pane.title" :closable="pane.closable">
+            <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+              <a-space>
+                <a-input placeholder="请输入接口地址" style="min-width: 110vh;">
+                  <template #addonBefore>
+                    <a-select v-model:value="apiType" style="width: 90px">
+                      <a-select-option value="POST">POST</a-select-option>
+                      <a-select-option value="GET">GET</a-select-option>
+                    </a-select>
+                  </template>
+                </a-input>
+                <a-button type="primary" @click="importStart">开始导入</a-button>
+              </a-space>
+              <a-flex :vertical="value === 'vertical'">
+                <RequestParamMapper ref="RequestComponent"/>
+                <ImportResponse/>
+              </a-flex>
+            </div>
+          </a-tab-pane>
+        </a-tabs>
+
 
       </a-layout-content>
       <a-layout-footer style="text-align: center">
@@ -94,6 +75,26 @@ const RequestComponent = ref();
 const collapsed = ref(false);
 const selectedKeys = ref(['1']);
 const apiType = ref('GET')
+const menuItems = ref([
+  {
+    key: 'sub1',
+    title: '文件夹1',
+    children: [
+      {
+        title: '页面 1',
+        key: 'setting:1',
+      },
+      {
+        title: '页面 2',
+        key: 'setting:2',
+      }
+    ],
+  }, {
+    key: 'sub1',
+    title: '页面3'
+  }
+]);
+
 
 // 引用子组件
 const importStart = async () => {
@@ -122,6 +123,39 @@ const panes = ref([
     closable: false,
   },
 ]);
+const activeKey = ref(panes.value[0].key);
+const newTabIndex = ref(0);
+const add = () => {
+  activeKey.value = `newTab${++newTabIndex.value}`;
+  panes.value.push({
+    title: 'New Tab',
+    content: 'Content of new Tab',
+    key: activeKey.value,
+  });
+};
+const remove = targetKey => {
+  let lastIndex = 0;
+  panes.value.forEach((pane, i) => {
+    if (pane.key === targetKey) {
+      lastIndex = i - 1;
+    }
+  });
+  panes.value = panes.value.filter(pane => pane.key !== targetKey);
+  if (panes.value.length && activeKey.value === targetKey) {
+    if (lastIndex >= 0) {
+      activeKey.value = panes.value[lastIndex].key;
+    } else {
+      activeKey.value = panes.value[0].key;
+    }
+  }
+};
+const onEdit = (targetKey, action) => {
+  if (action === 'add') {
+    add();
+  } else {
+    remove(targetKey);
+  }
+};
 </script>
 <style scoped>
 .logo {
@@ -137,23 +171,24 @@ const panes = ref([
 .ant-layout-sider-trigger {
   background: #fff;
 }
+
 .a-layout-sider-trigger {
   background-color: #f0f0f0 !important; /* 与 light 主题保持一致 */
-  color: #000 !important;              /* 设置文字颜色 */
-  border-top: 1px solid #d9d9d9;       /* 添加边框 */
+  color: #000 !important; /* 设置文字颜色 */
+  border-top: 1px solid #d9d9d9; /* 添加边框 */
 }
 
 
 .ant-layout .ant-layout-sider-trigger {
-   position: fixed;
-   bottom: 0;
-   z-index: 1;
-   height: 48px;
-   color: pink !important;
-   line-height: 48px;
-   text-align: center;
-   background: #fff !important;
-   cursor: pointer;
-   transition: all 0.2s;
- }
+  position: fixed;
+  bottom: 0;
+  z-index: 1;
+  height: 48px;
+  color: pink !important;
+  line-height: 48px;
+  text-align: center;
+  background: #fff !important;
+  cursor: pointer;
+  transition: all 0.2s;
+}
 </style>
